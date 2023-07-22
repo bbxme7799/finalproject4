24f83b168c105e229988e448a591124c346dea97
Ninym2545 <Ninym250245@gmail.com>
2023-07-22 21:15:48 +0700

    create history page

--- pages/users/history/index.jsx
+++ pages/users/history/index.jsx
@@ -119,25 +119,25 @@ const Users = [
 
 
 function index() {
-
+    // ส่วน Search
     const [query, setquery] = useState("");
     const [btn, setbtn] = useState("");
+    // ส่วน pagination 
     const [currentPage, setcurrentPage] = useState(1);
-    const recordsPerPage = 10;
+    const [recordsPerPage , setrecordsPerPage] = useState(10);
     const lastIndex = currentPage * recordsPerPage;
     const firstIndex = lastIndex - recordsPerPage;
     const records = Users.slice(firstIndex, lastIndex);
     const npage = Math.ceil(Users.length / recordsPerPage);
     const number = [...Array(npage + 1).keys()].slice(1);
-
-
+   
+    // Acive Button
     useEffect(() => {
         $(document).on('click', '.sectionli', function () {
             $(this).addClass('active').siblings().removeClass('active')
         })
     }, [])
 
-    console.log(btn)
     // console.log(Users.filter(user => user.first_name.toLocaleLowerCase.includes("Em")))
     return (
         <div className="ml-[255px] mt-[65px] h-auto">
@@ -153,7 +153,7 @@ function index() {
                         <div className="border-b-2 border-gray-200">
                             <ul className={styles.section}>
                                 <li className="sectionli mx-1 my-3 bg-gray-50 text-gray-500 px-5 py-2 rounded-lg  active" onClick={(e) => setbtn(e.target.value)} value=""><button onClick={(e) => setbtn(e.target.value)} value="">All</button></li>
-                                <li className="sectionli mx-1 my-3 bg-gray-50 text-gray-500 px-5 py-2 rounded-lg " onClick={(e) => setbtn(e.target.value)} value="โปรดรอ.."><button onClick={(e) => setbtn(e.target.value)} value="โปรดรอใใ">โปรดรอ..</button></li>
+                                <li className="sectionli mx-1 my-3 bg-gray-50 text-gray-500 px-5 py-2 rounded-lg " onClick={(e) => setbtn(e.target.value)} value="โปรดรอ.."><button onClick={(e) => setbtn(e.target.value)} value="โปรดรอ..">โปรดรอ..</button></li>
                                 <li className="sectionli mx-1 my-3 bg-gray-50 text-gray-500 px-5 py-2 rounded-lg  " onClick={(e) => setbtn(e.target.value)} value="ดำเนินการ"><button onClick={(e) => setbtn(e.target.value)} value="ดำเนินการ">ดำเนินการ</button></li>
                                 <li className="sectionli mx-1 my-3 bg-gray-50 text-gray-500 px-5 py-2 rounded-lg " onClick={(e) => setbtn(e.target.value)} value="เสร็จสิ้น"><button onClick={(e) => setbtn(e.target.value)} value="เสร็จสิ้น">เสร็จสิ้น</button></li>
                                 <li className="sectionli mx-1 my-3 bg-gray-50 text-gray-500 px-5 py-2 rounded-lg " onClick={(e) => setbtn(e.target.value)} value="คืนบางส่วน"><button onClick={(e) => setbtn(e.target.value)} value="คืนบางส่วน">คืนบางส่วน</button></li>
@@ -169,8 +169,8 @@ function index() {
                         <div className="w-full my-5 mx-5 px-5 py-5">
                             <table className="w-[98%]">
                                 <tbody>
-                                    <tr>
-                                        <th className="w-[5%] text-left">ID</th>
+                                    <tr className="border-b-2">
+                                        <th className="w-[5%] text-left p-4">ID</th>
                                         <th className="w-[25%] text-left">บริการ</th>
                                         <th className="w-[40%] text-left">ลิงก์</th>
                                         <th className="w-[5%] text-center">ค่าใช้จ่าย</th>
@@ -180,14 +180,14 @@ function index() {
                                         <th className="w-[5%] text-center">เพิ่มเติม</th>
                                     </tr>
                                     {records.filter((user) => user.status.toLowerCase().includes(query || btn)).map((item) => (
-                                        <tr key={item.id}  >
+                                        <tr key={item.id} className="border-b-2"  >
                                             <td className="text-left"><p className="mx-2 my-3">{item.id}</p></td>
                                             <td className="text-left"><p className="my-3">{item.service}</p></td>
                                             <td className="text-left"><p className="my-3">{item.src}</p></td>
                                             <td className="text-center"><p className="my-3">{item.cost}</p></td>
                                             <td className="text-center"><p className="my-3">{item.start}</p></td>
                                             <td className="text-center"><p className="my-3">{item.count}</p></td>
-                                            <td className="text-center"><p className="my-3">{item.status}</p></td>
+                                            <td className="text-center"><p className="my-3 w-24 mx-auto rounded-md py-1 text-white " style={{ backgroundColor: getStatusColor(item.status)  }}>{item.status}</p></td>
                                             <td className="text-center"><p className="my-3">{item.more}</p></td>
 
                                         </tr>
@@ -195,26 +195,36 @@ function index() {
                                 </tbody>
                             </table>
 
-                            <div>
+                            <div className="flex justify-between mx-3 my-5 ">
+                                <div className="my-auto flex items-center">
+                                <label for="dropdownMenu" className="pr-4">แสดง</label>
+                                <select id="dropdownMenu" name="dropdownMenu" className="mt-[0.2rem]  border-2 rounded-md py-2 px-3"  onClick={(e) => setrecordsPerPage(e.target.value)}>
+                                    <option value="10">10</option>
+                                    <option value="25">25</option>
+                                    <option value="50">50</option>
+                                </select>
+                                <label for="dropdownMenu" className="pl-4 flex">แถว <p className="ml-3 opacity-70">จากทั้งหมด {Users.length} ข้อมูล</p></label>
+                                </div>
+
                                 <nav>
                                     <ul className="flex justify-end mx-1 my-5">
                                         <li className="page-item mx-1 border-2 rounded-l-xl px-2 py-1">
-                                        <a href="#" className="page-link" onClick={prePage} >
-                                            Prev
-                                        </a>
+                                            <a href="#" className="page-link" onClick={prePage} >
+                                                ก่อนหน้า
+                                            </a>
                                         </li>
                                         {
-                                            number.map((n, i ) => (
+                                            number.map((n, i) => (
                                                 <li className={`page-item mx-1 px-3 text-center items-center rounded-md ${currentPage === n ? 'active' : ''}`} key={i}>
-                                                    <a href="#" className="page-link flex justify-items-center mt-1" 
-                                                    onClick={() => changeCPage(n)}>{n}</a>
+                                                    <a href="#" className="page-link flex justify-items-center mt-1"
+                                                        onClick={() => changeCPage(n)}>{n}</a>
                                                 </li>
                                             ))
                                         }
-                                         <li className="page-item mx-1 border-2 rounded-r-xl px-2 py-1">
-                                        <a href="#" className="page-link" onClick={nextPage} >
-                                            Next
-                                        </a>
+                                        <li className="page-item mx-1 border-2 rounded-r-xl px-2 py-1">
+                                            <a href="#" className="page-link" onClick={nextPage} >
+                                                ถัดไป
+                                            </a>
                                         </li>
                                     </ul>
                                 </nav>
@@ -228,17 +238,39 @@ function index() {
         </div>
     );
 
+    // Style button color
+    
+    function getStatusColor(status) {
+        switch (status) {
+          case "โปรดรอ..":
+            return "#008CBA"
+          case "ดำเนินการ":
+            return "#F4D03F";
+          case "เสร็จสิ้น":
+            return "#4CAF50";
+          case "คืนบางส่วน":
+            return "#E67E22";
+          case "ประมวลผล":
+            return "#E67E22";
+          case "ยกเลิก":
+            return "#f44336";
+          default:
+            return "black";
+        }
+      }
+      
+
     function nextPage() {
-        if(currentPage !== lastIndex){
+        if (currentPage !== lastIndex) {
             setcurrentPage(currentPage + 1)
         }
     }
     function prePage() {
-        if(currentPage !== firstIndex){
+        if (currentPage !== firstIndex) {
             setcurrentPage(currentPage - 1);
         }
     }
-    function changeCPage(id){
+    function changeCPage(id) {
 
     }
 
