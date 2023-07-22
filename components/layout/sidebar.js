1d77d566dcf20bb1a78c4378b9f008cd34421e2d
bbxme7799 <63011211102@msu.ac.th>
2023-07-22 22:12:57 +0700

    My message

--- components/layout/sidebar.js
+++ components/layout/sidebar.js
@@ -49,7 +49,7 @@ export default function Sidebar() {
             </li>
             <li>
               <Link
-                href="users/history"
+                href="/users/history"
                 className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
               >
                 <span className="inline-flex justify-center items-center ml-4">
